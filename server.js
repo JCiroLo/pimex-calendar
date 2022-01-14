596a1b60a9c92c11ccde89cadc6a6e5b58ca5082
Juan P Ciro <juanciro35@gmail.com>
2022-01-14 12:04:19 -0500

    Startic server for heroku definitive

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const serveStatic = require('serve-static')
-// const path = require('path')
-app = express()
-const distDir = __dirname + '/dist/'
-app.use(serveStatic(distDir))
-const port = process.env.PORT || 3000
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+// All urls goto to index.html in /dist folder [build folder]
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+const port = process.env.PORT || 8081
 app.listen(port)
+console.log(`App is listening on port: ${port}`)
